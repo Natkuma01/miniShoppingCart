fa6b13ab7163e71112d2d6c01e5aa5e35aa14eb0
Natalie Chan <natkuma@outlook.com>
2025-03-29 06:17:28 -0400

    add userinput

--- main.cpp
+++ main.cpp
@@ -1,44 +1,64 @@
 #include <iostream>
 #include <sstream>
+#include <cctype>
+#include <algorithm>
 using namespace std;
 
 #include "ItemToPurchase.h"
 
 int main() {
    ItemToPurchase item1;
-    ItemToPurchase item2;
-    string userinput;
-    string name;
-    int price;
-    int quantity;
+   ItemToPurchase item2;
+   string userinput;
+   string name;
+   int price;
+   int quantity;
+    
+   cout << "Item 1" << endl;
+   cout << "Enter the item name:" << endl;
 
    getline(cin, name);
-    item1.SetName(name);
-    
-    getline(cin, userinput);
-    stringstream(userinput) >> price;
-    item1.SetPrice(price);
-    
-    getline(cin, userinput);
-    stringstream(userinput) >> quantity;
-    item1.SetQuantity(quantity);
+   item1.SetName(name);
+   
+   cout << "Enter the item price:" << endl; 
+   getline(cin, userinput);
+   userinput.erase(remove_if(userinput.begin(),userinput.end(), ::isspace),userinput.end());
+   stringstream(userinput) >> price;
+   item1.SetPrice(price);
+   
+   cout << "Enter the item quantity:" << endl; 
+   getline(cin, userinput);
+   stringstream(userinput) >> quantity;
+   item1.SetQuantity(quantity);
 
-    cin.ignore(); 
+   
+   cout << "\n";
+   cout << "Item 2" << endl;
 
     // Item 2
-    getline(cin, userinput);
-    item2.SetName(userinput);
-    getline(cin, userinput);
-    stringstream(userinput) >> price;
-    item2.SetPrice(price);
-    getline(cin, userinput);
-    stringstream(userinput) >> quantity;
-    item2.SetQuantity(quantity);
+   cout << "Enter the item name:" << endl;
+   getline(cin, name);
+   item2.SetName(name);
+   
+   cout << "Enter the item price:" << endl; 
+   getline(cin, userinput);
+   userinput.erase(remove_if(userinput.begin(),userinput.end(), ::isspace),userinput.end());
+   stringstream(userinput) >> price;
+   item2.SetPrice(price);
+   
+   cout << "Enter the item quantity:" << endl;
+   getline(cin, userinput);
+   stringstream(userinput) >> quantity;
+   item2.SetQuantity(quantity);
+   
+   cout << "\n";
 
-    cout << "TOTAL COST" << endl;
-    item1.Print();
-    item2.Print();
-
+   cout << "TOTAL COST" << endl;
+   item1.Print();
+   item2.Print();
+   
+   cout << "\n";
+   
    cout << "Total: $" << item1.GetQuantity() * item1.GetPrice() + item2.GetQuantity() * item2.GetPrice() << endl;
    
    return 0;
